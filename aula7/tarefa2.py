ae433f40e9fbc1c7244b42b90c17b176f6bbba72
David Vilaça <vilacapdavid@gmail.com>
2017-09-15 08:30:38 -0300

    tarefa 2 e 3 - aula 7

---
+++ aula7/tarefa2.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 09:48:18 2017
+
+@author: davidpvilaca
+"""
+#import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def track(img):
+
+    '''Accepts BGR image as Numpy array
+       Returns: (x,y) coordinates of centroid if found
+                (-1,-1) if no centroid was found
+                None if user hit ESC
+    '''
+    
+    image = img.copy()
+
+    # Blur the image to reduce noise
+    blur = cv2.GaussianBlur(image, (5,5),0)
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    # Threshold the HSV image for only green colors
+    lower_green = np.array([40,70,70])
+    upper_green = np.array([80,200,200])
+
+    # Threshold the HSV image to get only green colors
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    
+    # Blur the mask
+    bmask = cv2.GaussianBlur(mask, (5,5),0)
+
+    # Take the moments to get the centroid
+    moments = cv2.moments(bmask)
+    m00 = moments['m00']
+    centroid_x, centroid_y = None, None
+    if m00 != 0:
+        centroid_x = int(moments['m10']/m00)
+        centroid_y = int(moments['m01']/m00)
+
+    # Assume no centroid
+    ctr = (-1,-1)
+
+    # Use centroid if it exists
+    if centroid_x != None and centroid_y != None:
+
+        ctr = (centroid_x, centroid_y)
+
+        # Put black circle in at centroid in image
+        cv2.circle(image, ctr, 4, (0,0,0))
+
+    cv2.imwrite('saida_bolaverde.jpg', image)
+    # Display full-color image
+    cv2.imshow('BOLA VERDE', image)
+    if cv2.waitKey(): cv2.destroyAllWindows()
+
+    # Force image display, setting centroid to None on ESC key input
+    if cv2.waitKey(1) & 0xFF == 27:
+        ctr = None
+    
+    # Return coordinates of centroid
+    return ctr
+
+def main():
+    
+    track(cv2.imread('bolaverde.jpg'))
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
