0b8ca7563a1c4df9772a7295617947b9fc15e6a2
David Vilaça <vilacapdavid@gmail.com>
2017-11-08 21:20:39 -0200

    adicionado material aula 12

---
+++ aula12/tarefa.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 08 23:19:06 2017
+
+@author: davidpvilaca
+"""
+import matplotlib.pyplot as plt
+import cv2
+import math
+import numpy as np
+
