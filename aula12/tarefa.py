ce9963621ed565f58af5ced7916be15cc6495cb7
David Vilaça <vilacapdavid@gmail.com>
2017-11-10 08:58:59 -0200

    exercicios resolvidos da aula12 e aula8

--- aula12/tarefa.py
+++ aula12/tarefa.py
@@ -7,6 +7,66 @@ Created on Wed Nov 08 23:19:06 2017
 """
 import matplotlib.pyplot as plt
 import cv2
-import math
 import numpy as np
+from scipy.ndimage import label
 
+def loadImg(path):
+    img = cv2.imread(path)
+    return (img, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+
+def binOtsu(img_gray):
+    img_bin = cv2.threshold(img_gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, np.ones((3, 3), dtype=int))
+    return img_bin
+
+def showImg(img, cmap=None):
+    plt.figure()
+    plt.imshow(img, cmap=cmap)
+    
+"""
+@param {} a
+@param {cv2 image} img
+"""
+def segment_on_dt(a, img):
+    border = cv2.dilate(img, None, iterations=5)
+    border = border - cv2.erode(border, None)
+    cv2.imwrite('result1.png', border)
+    dt = cv2.distanceTransform(img, 2, 3)
+    dt = ((dt - dt.min()) / (dt.max() - dt.min()) * 255).astype(np.uint8)
+    _, dt = cv2.threshold(dt, 180, 255, cv2.THRESH_BINARY)    
+    lbl, ncc = label(dt)
+    cv2.imwrite('result2.png', lbl)
+    contours1 = cv2.findContours(lbl.astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+    lbl = lbl * (255/ncc)
+    lbl[border == 255] = 255
+    cv2.imwrite('result3.png', lbl)
+    lbl = lbl.astype(np.int32)
+    cv2.watershed(a, lbl)
+    cv2.imwrite('result4.png', lbl)
+    lbl[lbl == -1] = 0
+
+    lbl = lbl.astype(np.uint8)
+    seg = 255 - lbl
+    
+    showImg(seg, plt.cm.Greys_r)
+    contours2 = cv2.findContours(seg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+    return seg, np.max([len(contours1), len(contours2)])
+
+
+def main():
+    
+    paths = ['laranjas.jpg', 'ovos.jpg', 'patologia1.jpg', 'aspirina.jpg']
+    #paths = ['patologia1.jpg']
+    
+    for path in paths:
+        img, gray = loadImg(path)
+        result, num = segment_on_dt(img.copy(), binOtsu(gray))    
+        #showImg(binOtsu(gray), plt.cm.Greys_r)
+        showImg(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title(str(num))
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
--- aula8/tarefa2.py
+++ aula8/tarefa2.py
@@ -10,32 +10,36 @@ import numpy as np
 import cv2
 
 DIGITS_LOOKUP = {
+    #0  1  2  3  4  5  6
     (1, 1, 1, 0, 1, 1, 1): 0,
     (0, 0, 1, 0, 0, 1, 0): 1,
-    (1, 0, 1, 1, 1, 1, 0): 2,
+    (0, 1, 0, 0, 1, 0, 0): 1,
+    (1, 0, 1, 1, 1, 0, 1): 2,
     (1, 0, 1, 1, 0, 1, 1): 3,
+    (1, 0, 0, 1, 0, 0, 1): 3,
+    (1, 0, 1, 1, 0, 0, 1): 3,
     (0, 1, 1, 1, 0, 1, 0): 4,
     (1, 1, 0, 1, 0, 1, 1): 5,
     (1, 1, 0, 1, 1, 1, 1): 6,
     (1, 0, 1, 0, 0, 1, 0): 7,
+    (1, 0, 1, 0, 0, 0, 0): 7,
     (1, 1, 1, 1, 1, 1, 1): 8,
-    (1, 1, 1, 1, 0, 1, 1): 9
+    (1, 1, 1, 1, 0, 1, 1): 9,
+    (1, 1, 1, 1, 0, 1, 0): 9
 }
 
-
-def main():
+def segmentDigits(path, typeThresh = cv2.THRESH_BINARY):
     
-    img = cv2.imread('ex.png', cv2.IMREAD_GRAYSCALE)
+    img_o = cv2.imread(path)
+    img = cv2.cvtColor(img_o.copy(), cv2.COLOR_BGR2GRAY)
     
-    thresh1 = cv2.threshold(img, 0, 255,	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh1 = cv2.threshold(img, 0, 255,	typeThresh + cv2.THRESH_OTSU)[1]
     
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
     thresh2 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel) # abertura
     closing = cv2.morphologyEx(thresh2, cv2.MORPH_CLOSE, kernel) # fechamento
     
-    
-    cnts1 = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts1[1]
+    cnts = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
     digitCnts = []
     
     # loop over the digit area candidates
@@ -50,12 +54,16 @@ def main():
     #plt.imshow(img, cmap=plt.cm.Greys_r)
     
     digits = []
+    rois_w_avg = 0
     
     # loop over each of the digits
     for c in digitCnts:
         # extract the digit ROI
         (x, y, w, h) = cv2.boundingRect(c)
         roi = thresh2[y:y + h, x:x + w]
+        
+        #plt.figure()
+        #plt.imshow(roi, cmap=plt.cm.Greys_r)
         # compute the width and height of each of the 7 segments
         (roiH, roiW) = roi.shape
         (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
@@ -77,19 +85,41 @@ def main():
             area = (xB - xA) * (yB - yA)
             # if the total number of non-zero pixels is greater than
             # 50% of the area, mark the segment as "on"
-            if total / float(area) > 0.5:
+            if (total / float(area)) >= 0.4:
                 on[i]= 1
         # lookup the digit and draw it on the image
-        digit = DIGITS_LOOKUP[tuple(on)]
+        lookup = tuple(on)
+        digit = DIGITS_LOOKUP[lookup] if lookup in DIGITS_LOOKUP else ''
+        if ( rois_w_avg != 0 and (w/rois_w_avg) < 0.5 ):
+            digit = 1
+        else:
+            rois_w_avg = h if rois_w_avg == 0 else np.average([rois_w_avg, w])
         digits.append(digit)
-        print(tuple(on))
-        print(digit)
         
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        cv2.putText(img, str(digit), (x, y +20),
+        #print(str(lookup) + ': ' + str(digit))
+        
+        #print(tuple(on))
+        #print(digit)
+        
+        cv2.rectangle(img_o, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        cv2.putText(img_o, str(digit), (x, y +20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
     
-    #plt.imshow(img, cmap=plt.cm.Greys_r)
+    plt.figure()
+    plt.imshow(cv2.cvtColor(img_o, cv2.COLOR_BGR2RGB))
+    
+
+
+def main():
+    
+    inputs = [ 
+        { 'path': 'ex.png', 'type': cv2.THRESH_BINARY_INV },
+        { 'path': 'ex2.png', 'type': cv2.THRESH_BINARY },
+        { 'path': 'ex3.png', 'type': cv2.THRESH_BINARY }
+    ]
+    
+    for i in inputs:
+        segmentDigits(i['path'], i['type'])
     
     return 0
 
